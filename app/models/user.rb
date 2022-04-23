ce0f95e5e13371736260b1fb110641e1fe994aad
Artem Solonyi <igorartem123@gmail.com>
2022-04-23 18:33:35 +0300

    Customize styles for profile doctor

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,28 +4,29 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :patient,dependent: :destroy
-  has_many :doctor,dependent: :destroy
+  has_many :patient, dependent: :destroy
+  has_many :doctor, dependent: :destroy
   enum role: [:patient, :doctor, :admin]
 
+  def get_recommendation(doctor_id, patient_id)
+    if Recommendation.exists?(doctor_id: doctor_id, patient_id: patient_id)
+      Recommendation.find_by(doctor_id: doctor_id, patient_id: patient_id).recommendation
+    end
+    end
 
-  def get_recommendation(doctor_id,patient_id)
-    "re"
+    after_initialize :set_default_role, if: :new_record?
+
+    def set_default_role
+      self.role ||= :user
+    end
+
+    validates :phone_number, uniqueness: true
+
+    def email_required?
+      false
+    end
+
+    def email_changed?
+      false
+    end
   end
-
-  after_initialize :set_default_role, if: :new_record?
-
-  def set_default_role
-    self.role ||= :user
-  end
-
-  validates :phone_number, uniqueness: true
-
-  def email_required?
-    false
-  end
-
-  def email_changed?
-    false
-  end
-end
