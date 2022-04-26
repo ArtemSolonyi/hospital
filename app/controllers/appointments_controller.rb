b8739c709e99002c3feb4fce68a4334d5f86117d
Artem Solonyi <igorartem123@gmail.com>
2022-04-26 11:23:38 +0300

    Ready for full deploy

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -9,13 +9,13 @@ class AppointmentsController < ApplicationController
 
     if  Doctor.find(@doctor_id).appointments.count < 10 && !found_appointment
 
-      @messages = "Вы успешно записаны"
+      @messages = "You have successfully created an appointment"
       @appointment = Appointment.create!(patient_id: patient, doctor_id: @doctor_id)
 
     elsif found_appointment
-      @messages = "Вы уже записаны"
+      @messages = "You have already created an appointment"
     else
-      @messages = "Простите, но у доктора всё занято"
+      @messages = "I'm sorry, but the doctor is busy"
     end
     respond_to do |format|
       format.js { render partial: 'layouts/messages' }
@@ -26,9 +26,5 @@ class AppointmentsController < ApplicationController
     @appointment = Appointment.new
   end
 
-  def set_recommendation
-    appa = Appointment.joins(:patient,:doctor).where(appointments:{patient_id:params[:patient_id],doctor_id:params[:doctor_id]}).update(recommendation:params[:recommendation],status:'off')[0]
-    appa.save
-  end
 
 end
